f3c5ed745d7def7735374dfb50ba3a430568e7a2
mahesh9150 <nmagesh24082000@gmail.com>
2026-08-28 10:58:41 +0530

    route structures

--- main.js
+++ main.js
@@ -9,10 +9,29 @@ app.get('/', (req,res) => {
     })
 
 });
+// crud functionality of movies
+//for reading mvies
+app.get('/movies', () =>{
+
+})
+//for creating movies
+app.post('/movies', () =>{
+    
+})
+//u- for updating movies
+app.put('/movies/:id', ()=>{
+
+})
+//D - for delete movies
+app.delete('/movies/:id', () =>{
+    
+})
+
 
 app.listen(PORT, ()=>{
     console.log(`The server is running at http://localhost:${6969}`);
     
 });
+
 
 
